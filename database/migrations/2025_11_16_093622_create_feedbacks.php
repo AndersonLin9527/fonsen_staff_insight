4566f8505e1b7b3134ff9eea8bec74288e4cc31a
Anderson <henxo9527@gmail.com>
2025-11-16 20:18:51 +0800

    專案 | Migration | 修正 feedbacks 的 memo, start_at, close_at 為必填

--- database/migrations/2025_11_16_093622_create_feedbacks.php
+++ database/migrations/2025_11_16_093622_create_feedbacks.php
@@ -18,10 +18,10 @@ return new class extends Migration {
             $table->id();
             $table->string('code', 50)->unique()->comment('回饋代號');
             $table->string('title', 100)->comment('回饋主題');
-            $table->text('memo')->nullable()->comment('回饋備註');
-            $table->timestamp('start_at')->nullable()->comment('開始於');
-            $table->timestamp('close_at')->nullable()->comment('結束於');
-            $table->tinyInteger('status')->default(1)->comment('狀態');
+            $table->text('memo')->default('')->comment('回饋備註');
+            $table->timestamp('start_at')->comment('開始於');
+            $table->timestamp('close_at')->comment('結束於');
+            $table->tinyInteger('status')->comment('狀態');
             $table->timestamps();
         });
         // table 備註
